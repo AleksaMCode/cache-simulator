c04d8c951c2c3cbaf229ae2570defc3335a235aa
Aleksa Majkic <Majkic.A@salzgitter-ag.de>
2021-08-16 12:47:25 +0200

    SetLengths and GetBinaryAddress methods added.

--- CacheSimulation/Cache.cs
+++ CacheSimulation/Cache.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CacheSimulation
 {
@@ -26,11 +28,11 @@ namespace CacheSimulation
         public int CacheMisses { get; set; } = 0;
         public int MemoryReads { get; set; } = 0;
         public int MemoryWrites { get; set; } = 0;
-        public int NumLines { get; set; } = 0;
+        public int NumberOfLines { get; set; } = 0;
         public int Size { get; set; } = 0;
         public int Associativity { get; set; } = 0;
-        public int BlockNumber { get; set; } = 0;
-        public int SetNumber { get; set; } = 0;
+        public int BlockOffsetLength { get; set; } = 0;
+        public int SetIndexLength { get; set; } = 0;
         public CacheConfiguration CacheConfig { get; set; } = new CacheConfiguration();
 
         public void CreateColdCache(int numberOfLines)
@@ -40,7 +42,47 @@ namespace CacheSimulation
 
         public int GetTagLength(BitArray address)
         {
-            return address.Length - SetNumber - BlockNumber;
+            return address.Length - SetIndexLength - BlockOffsetLength;
+        }
+
+        /// <summary>
+        /// Sets the length of set index and block offset.
+        /// </summary>
+        public void SetLengths()
+        {
+            var i = 1;
+
+            for (; i < CacheConfig.BlockSize; i *= 2)
+            {
+                ++BlockOffsetLength;
+            }
+
+            if (i != CacheConfig.BlockSize && BlockOffsetLength != 0)
+            {
+                throw new Exception("Block size is not a power of 2.");
+            }
+
+            i = 1;
+            for (; i < NumberOfLines / Associativity; i *= 2)
+            {
+                ++SetIndexLength;
+            }
+
+            if (i != NumberOfLines / Associativity && SetIndexLength != 0)
+            {
+                throw new Exception("Associativity is not a power of 2.");
+            }
+        }
+
+        /// <summary>
+        /// Converts an address from hex format to binary format.
+        /// </summary>
+        /// <param name="address">Address in hex format.</param>
+        /// <returns>Address in binary format.</returns>
+        public string GetBinaryAddress(string address)
+        {
+            return string.Join(string.Empty,
+                address.Select(c => Convert.ToString(Convert.ToInt32(c.ToString(), 16), 2).PadLeft(4, '0')));
         }
     }
 }
