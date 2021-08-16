a805a793e62ba4a2fcd8d24531179eba67f57ccf
Aleksa Majkic <Majkic.A@salzgitter-ag.de>
2021-08-16 11:45:51 +0200

    Ram Generator added

--- RamGenerator2/Properties/AssemblyInfo.cs
+++ RamGenerator2/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6cd38e3a-b47b-4746-8241-f43547142850")]
+[assembly: Guid("6c6ddddf-d931-4744-81be-f991bcaa541a")]
 
 // Version information for an assembly consists of the following four values:
 //
