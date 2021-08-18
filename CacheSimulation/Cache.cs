f400dd125baf7a650e423dd6f7b19ca1b2d51df8
Aleksa Majkic <Majkic.A@salzgitter-ag.de>
2021-08-18 10:19:08 +0200

    Warning removed.

--- CacheSimulation/Cache.cs
+++ CacheSimulation/Cache.cs
@@ -2,10 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net;
-using System.Runtime.InteropServices.ComTypes;
 using System.Text;
-using System.Xml.Linq;
 
 namespace CacheSimulation
 {
