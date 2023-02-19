1afe0fa881f786191651221e7723253a4929d376
AleksaMCode <aleksamcode@gmail.com>
2023-02-19 02:26:52 +0100

    Removed commented-out code

--- CacheSimulator/CPU.cs
+++ CacheSimulator/CPU.cs
@@ -23,44 +23,6 @@ namespace CacheSimulator
             cores[coreNumber].SetTraceFileForL1(traceFileName);
         }
 
-        //public void Start()
-        //{
-        //    const int bufferSize = 4_096;
-        //    using var fileStream = File.OpenRead(L1.TraceFileName);
-        //    using var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, bufferSize);
-
-        //    string line;
-        //    var currentLine = -1;
-        //    while ((line = streamReader.ReadLine()) != null)
-        //    {
-        //        ++currentLine;
-        //        Instruction instruction;
-
-        //        try
-        //        {
-        //            instruction = L1.TraceLineParser(line);
-        //        }
-        //        catch (Exception)
-        //        {
-        //            continue;
-        //        }
-
-        //        if (instruction != null)
-        //        {
-        //            if (instruction.InstructionType == MemoryRelatedInstructions.Store)
-        //            {
-        //                var size = instruction.DataSize < L1.CacheConfig.BlockSize ? instruction.DataSize : L1.CacheConfig.BlockSize;
-        //                L1.WriteToCache(instruction.MemoryAddress, size, instruction.Data, out var _, currentLine);
-        //            }
-        //            else
-        //            {
-        //                var size = instruction.DataSize < L1.CacheConfig.BlockSize ? instruction.DataSize : L1.CacheConfig.BlockSize;
-        //                L1.ReadFromCache(instruction.MemoryAddress, size, out var _, currentLine);
-        //            }
-        //        }
-        //    }
-        //}
-
         public string ExecuteTraceLine(string traceLine, int traceIndex, int coreNumber)
         {
             return cores[coreNumber].ExecuteTraceLine(traceLine, traceIndex, coreNumber);
